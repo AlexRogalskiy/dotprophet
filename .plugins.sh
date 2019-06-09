2cae1fd6827d02ad14f0ed133c690c04ecf227bf
sumanthratna <sumanthratna@gmail.com>
2019-06-09 13:48:45 -0400

    Upload first commit

---
+++ .plugins.sh
@@ -0,0 +1,4 @@
+source /Users/suman/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-marzocchi-SLASH-zsh-notify/notify.plugin.zsh
+fpath+=( /Users/suman/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-marzocchi-SLASH-zsh-notify )
+source /Users/suman/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /Users/suman/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
