4c08b3d030a298adf9cc418b024e8056afac6eb0
sumanthratna <sumanthratna@gmail.com>
2020-03-08 18:14:04 -0400

    Fix antibody

--- .plugins.sh
+++ .plugins.sh
@@ -1,4 +1,2 @@
-source /Users/suman/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-marzocchi-SLASH-zsh-notify/notify.plugin.zsh
-fpath+=( /Users/suman/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-marzocchi-SLASH-zsh-notify )
 source /Users/suman/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
 fpath+=( /Users/suman/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
