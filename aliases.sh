9c9052a3647837f71c0bfa980b2bb00ed185868f
Sumanth Ratna <sumanthratna@gmail.com>
2020-06-25 21:45:34 -0400

    Add Zsh hashbang to executables
    This commit also removes cargo, invision-studio-app-dev, and matlab. The hashbangs may have resulted in a small performance boost.

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,5 @@
+#!/bin/env zsh
+
 for DOTFILE in $(find $(dirname ${(%):-%N})/aliases -maxdepth 1 -perm +111 -type f -or -type l)
 do
   [ -f "$DOTFILE" ] && source "$DOTFILE"
--- prompt.sh
+++ prompt.sh
@@ -1,3 +1,5 @@
+#!/bin/env zsh
+
 export CLICOLOR=1
 
 for DOTFILE in $(find $(dirname ${(%):-%N})/prompt -maxdepth 1 -perm +111 -type f -or -type l)
